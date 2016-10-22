862853f400830a93e661dd8c10359070b9db4db4
guptagaurav0075 <guptagaurav0075@gmail.com>
2016-10-22 13:51:37 -0700

    Add files via upload

---
+++ src/configs/config.php
@@ -0,0 +1,31 @@
+<?php
+//use cbd;
+$servername = "localhost";
+$username = "root";
+$password = "gauravabc";
+//$dbName = "HW3";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	else
+		echo "connection successful";
+/*	//$query[] ="CREATE TABLE Genere(gid int UNSIGNED AUTO_INCREMENT , Description VARCHAR(20) NOT NULL, PRIMARY KEY(gid) );";
+	//$query[] =  "INSERT INTO Genere VALUES (null,'FICTION');";
+	$query1 = cdb.getQuery();
+	foreach($query1 as $que){
+		if ($conn->query($que) === TRUE) {
+			echo "successfully";
+		} else {
+			echo "Error creating database: " . $conn->error;
+			break;
+		}
+	}
+
+$conn->close();
+*/
+?>
