7ffac27e75ebdf5c3669362c3e890539f4d1e749
guptagaurav0075 <guptagaurav0075@gmail.com>
2016-10-23 01:25:25 -0700

    Update config.php

--- src/configs/config.php
+++ src/configs/config.php
@@ -14,18 +14,4 @@ $conn = new mysqli($servername, $username, $password);
 	} 
 	else
 		echo "connection successful";
-/*	//$query[] ="CREATE TABLE Genere(gid int UNSIGNED AUTO_INCREMENT , Description VARCHAR(20) NOT NULL, PRIMARY KEY(gid) );";
-	//$query[] =  "INSERT INTO Genere VALUES (null,'FICTION');";
-	$query1 = cdb.getQuery();
-	foreach($query1 as $que){
-		if ($conn->query($que) === TRUE) {
-			echo "successfully";
-		} else {
-			echo "Error creating database: " . $conn->error;
-			break;
-		}
-	}
-
-$conn->close();
-*/
 ?>
