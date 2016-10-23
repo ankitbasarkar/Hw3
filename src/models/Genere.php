2bcec2cfbf45c6fca546d34f24d21e551c965bd0
guptagaurav0075 <guptagaurav0075@gmail.com>
2016-10-23 01:37:35 -0700

    Update Genere.php

--- src/models/Genere.php
+++ src/models/Genere.php
@@ -1,23 +1,21 @@
 <?php
 namespace GenereClass;
-use myconfig\config as italianretreat;
+use myconfig\config as DBConnect;
 include "../configs/config.php";
 
 class Genere{
 	//public $co = null;
-	public $coalminegaurav = null;
-	public $conn = null;
-
+	public $connection_Formed = null;
+	
 	function __construct(){
-		echo "constructor called";
-		$this->coalminegaurav= new italianretreat();
-		$this->coalminegaurav = $this->coalminegaurav->connect();
+		$this->connection_Formed= new DBConnect();
+		$this->connection_Formed = $this->connection_Formed->connect();
 		
 	}
 	function populateGenere(){
 		$result1 = array();
 		$count = 0;
-		$res = mysqli_query($this->coalminegaurav,"select GenereDescription from Genere");
+		$res = mysqli_query($this->connection_Formed,"select GenereDescription from Genere");
 		while($result = mysqli_fetch_assoc($res)){
 			$result1[$count] = $result['GenereDescription'];
 			$count++;
