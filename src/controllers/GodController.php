b0c05b89dcac216b4b8f9f1c0c6b0064728ba27b
guptagaurav0075 <guptagaurav0075@gmail.com>
2016-10-29 02:34:35 -0700

    Update GodController.php

--- src/controllers/GodController.php
+++ src/controllers/GodController.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ankit
- * Date: 10/22/2016
- * Time: 3:43 PM
- */
+
 
 namespace cool_name_for_your_group\hw3\controllers;
 require_once HW3ROOT."/src/views/LandingView.php";
