1502dce337ff06fff12b57f70833b2acb1034348
guptagaurav0075 <guptagaurav0075@gmail.com>
2016-10-31 16:35:27 -0700

    Update ratingList.php

--- src/views/helpers/ratingList.php
+++ src/views/helpers/ratingList.php
@@ -7,23 +7,23 @@ class ratingList extends Helper
 {
     //here data should be an array of Story Objects with id and title initialised
     function render($userRatingValue){
-        echo "Your Rating:";
+        echo "Your Rating:&nbsp;&nbsp;";
         if($userRatingValue[5] ==0){
-			for($index = 1; $index<6; $index++){
-				
+		for($index = 1; $index<6; $index++){
 				$href = "index.php?c=GodController&m=ReadParticularStory&Story_ID=".$userRatingValue[0]."&cValue=".$index."";
-				$linkValue = "<a href=".$href.">".$index."</a>";
+				$linkValue = "<a href=".$href.">".$index."&nbsp;&nbsp;&nbsp;</a>";
 				echo $linkValue;
-			}
-		}
-		else{
-			for($index = 1; $index<6; $index++){
-				if($index == $userRatingValue[5]){
-					echo "<b>".$index."</b>";
-				}
-				else
-					echo $index;
-			}
 		}
 	}
+	else{
+		for($index = 1; $index<6; $index++){
+			if($index == $userRatingValue[5]){
+				echo "<b>".$index."&nbsp;&nbsp;</b>";
+			}
+			else
+				echo $index."&nbsp;&nbsp;";
+			
+		}
+	}
+     }
 }
